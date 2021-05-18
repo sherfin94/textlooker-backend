76757d3192bc74389b0b4d669c356367b08b6959
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-05-19 01:39:19 +0530

    Verify source before saving text

--- controllers/text.go
+++ controllers/text.go
@@ -12,15 +12,15 @@ import (
 const ReferenceDate = "Jan 2 15:04:05 -0700 MST 2006"
 
 type Text struct {
-	Content  string `json:"content" validate:"required"`
+	Content  string   `json:"content" validate:"required"`
 	Author   []string `json:"author" validate:"required"`
-	Date     string `json:"date" validate:"required"`
-	SourceID int    `json:"sourceID" validate:"required"`
+	Date     string   `json:"date" validate:"required"`
+	SourceID int      `json:"sourceID" validate:"required"`
 }
 
 func PostText(context *gin.Context) {
 	var text Text
-	var source Source
+	var source models.Source
 
 	if err := context.ShouldBindJSON(&text); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -34,13 +34,13 @@ func PostText(context *gin.Context) {
 	}
 
 	user, _ := context.Get("user")
-	sourceSearchResult := database.Database.Where("user_id = ? and id = ?", user.(*models.User).ID, text.SourceID).Find(&source)
-	if sourceSearchResult.Error != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": sourceSearchResult.Error.Error()})
+	database.Database.Where("user_id = ? and id = ?", user.(*models.User).ID, text.SourceID).Find(&source)
+	if source.ID == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Source not found"})
 		return
 	}
 
-	if text, err := models.NewText(text.Content, text.Author, time, text.SourceID); err != nil {
+	if text, err := models.NewText(text.Content, text.Author, time, int(source.ID)); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	} else {
