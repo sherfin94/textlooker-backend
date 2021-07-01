82b42f1017a34de4684ca63942090ddafe863769
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-07-01 16:29:03 +0530

    Add insight params required for loading insights

--- controllers/insight.go
+++ controllers/insight.go
@@ -4,54 +4,29 @@ import (
 	"net/http"
 	"textlooker-backend/database"
 	"textlooker-backend/models"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+const insightDateFormat = "2006-01-02T15:04:05-07:00"
+
 type InsightParams struct {
-	Title          string `json:"title" binding:"required"`
-	SourceID       int    `json:"sourceID" binding:"required"`
-	Filter         string `json:"filter" binding:"required"`
-	LookForHandle  string `json:"lookForHandle" binding:"required"`
-	VisualizeTexts string `json:"visualizeTexts" binding:"required"`
+	Title              string `json:"title" binding:"required"`
+	SourceID           int    `json:"sourceID" binding:"required"`
+	Filter             string `json:"filter" binding:"required"`
+	LookForHandle      string `json:"lookForHandle" binding:"required"`
+	VisualizeTexts     string `json:"visualizeTexts" binding:"required"`
+	VisualizationType  string `json:"visualizationType" binding:"required"`
+	StartDate          string `json:"startDate" binding:"required"`
+	EndDate            string `json:"endDate" binding:"required"`
+	DateRangeAvailable bool   `json:"dateRangeAvailable"`
 }
 
 func PostInsight(context *gin.Context) {
 	var source models.Source
-	var insightParams InsightParams
-
-	if err := context.ShouldBindJSON(&insightParams); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	user, _ := context.Get("user")
-	database.Database.Where("user_id = ? and id = ?", user.(*models.User).ID, insightParams.SourceID).Find(&source)
-	if source.ID == 0 {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Source could not be validated"})
-		return
-	}
-
-	if insight, err := models.NewInsight(insightParams.Title, insightParams.Filter, insightParams.LookForHandle, insightParams.VisualizeTexts, int(source.ID)); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	} else {
-		context.JSON(http.StatusOK, gin.H{
-			"status":    "Insight created",
-			"insightID": insight.ID,
-		})
-	}
-}
-
-type GetInsightsParams struct {
-	SourceID int `json:"sourceID" binding:"required"`
-}
-
-func GetInsights(context *gin.Context) {
-	var params GetInsightsParams
-	var source models.Source
-	var insights []models.Insight
-	result := []map[string]interface{}{}
+	var params InsightParams
+	var startDate, endDate time.Time
 
 	if err := context.ShouldBindJSON(&params); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -65,15 +40,81 @@ func GetInsights(context *gin.Context) {
 		return
 	}
 
-	database.Database.Where("source_id = ?", source.ID).Order("updated_at desc").Find(&insights)
+	if params.DateRangeAvailable {
+		var err error
+		startDate, err = time.Parse(insightDateFormat, params.StartDate)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		endDate, err = time.Parse(insightDateFormat, params.EndDate)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	} else {
+		startDate = time.Now()
+		endDate = time.Now()
+	}
+
+	if insight, err := models.NewInsight(
+		params.Title,
+		params.Filter,
+		params.LookForHandle,
+		params.VisualizeTexts,
+		startDate,
+		endDate,
+		params.VisualizationType,
+		params.DateRangeAvailable,
+		int(source.ID),
+	); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		context.JSON(http.StatusOK, gin.H{
+			"status":    "Insight created",
+			"insightID": insight.ID,
+		})
+	}
+}
+
+type GetInsightsParams struct {
+	SourceID int `form:"sourceID" binding:"required"`
+}
+
+func GetInsights(context *gin.Context) {
+	var params GetInsightsParams
+	var source models.Source
+	var insights []models.Insight
+	result := []map[string]interface{}{}
+
+	if err := context.BindQuery(&params); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, _ := context.Get("user")
+	database.Database.Where("user_id = ? and id = ?", user.(*models.User).ID, params.SourceID).Find(&source)
+	if source.ID == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Source could not be validated"})
+		return
+	}
+
+	database.Database.Where("source_id = ?", source.ID).Order("updated_at desc").Order("updated_at desc").Find(&insights)
 
 	for _, insight := range insights {
 		result = append(result, map[string]interface{}{
-			"title":          insight.Title,
-			"id":             insight.ID,
-			"filter":         insight.Filter,
-			"lookForHandle":  insight.LookForHandle,
-			"visualizeTexts": insight.VisualizeTexts,
+			"last_updated":       insight.UpdatedAt.Format(insightDateFormat),
+			"title":              insight.Title,
+			"id":                 insight.ID,
+			"filter":             insight.Filter,
+			"lookForHandle":      insight.LookForHandle,
+			"visualizeTexts":     insight.VisualizeTexts,
+			"startDate":          insight.StartDate.Format(insightDateFormat),
+			"endDate":            insight.EndDate.Format(insightDateFormat),
+			"dateRangeAvailable": insight.DateRangeAvailable,
+			"visualizationType":  insight.VisualizationType,
 		})
 	}
 
@@ -83,8 +124,8 @@ func GetInsights(context *gin.Context) {
 }
 
 type DeleteInsightsParams struct {
-	SourceID  int `json:"sourceID" binding:"required"`
-	InsightID int `json:"insightID" binding:"required"`
+	SourceID  int `form:"sourceID" binding:"required"`
+	InsightID int `form:"insightID" binding:"required"`
 }
 
 func DeleteInsight(context *gin.Context) {
@@ -92,7 +133,7 @@ func DeleteInsight(context *gin.Context) {
 	var source models.Source
 	var insight models.Insight
 
-	if err := context.ShouldBindJSON(&params); err != nil {
+	if err := context.BindQuery(&params); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
--- models/insight.go
+++ models/insight.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"textlooker-backend/database"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
@@ -9,11 +10,15 @@ import (
 
 type Insight struct {
 	gorm.Model
-	Title          string `gorm:"not null" validate:"required"`
-	SourceID       int    `gorm:"not null"`
-	Filter         string `gorm:"not null" validate:"required"`
-	LookForHandle  string `gorm:"not null" validate:"required"`
-	VisualizeTexts string `gorm:"not null" validate:"required"`
+	Title              string    `gorm:"not null,unique" validate:"required"`
+	SourceID           int       `gorm:"not null"`
+	Filter             string    `gorm:"not null" validate:"required"`
+	LookForHandle      string    `gorm:"not null" validate:"required"`
+	VisualizeTexts     string    `gorm:"not null" validate:"required"`
+	VisualizationType  string    `gorm:"not null" validate:"required"`
+	StartDate          time.Time `gorm:"not null" validate:"required"`
+	EndDate            time.Time `gorm:"not null" validate:"required"`
+	DateRangeAvailable bool      `gorm:"not null"`
 }
 
 func (insight *Insight) BeforeSave(database *gorm.DB) (err error) {
@@ -27,13 +32,17 @@ func (insight *Insight) BeforeSave(database *gorm.DB) (err error) {
 	return nil
 }
 
-func NewInsight(title string, filter string, lookForHandle string, visualizeTexts string, sourceID int) (*Insight, error) {
+func NewInsight(title string, filter string, lookForHandle string, visualizeTexts string, startDate time.Time, endDate time.Time, visualizationType string, dateRangeAvailable bool, sourceID int) (*Insight, error) {
 	insight := &Insight{
-		Title:          title,
-		LookForHandle:  lookForHandle,
-		VisualizeTexts: visualizeTexts,
-		Filter:         filter,
-		SourceID:       sourceID,
+		Title:              title,
+		LookForHandle:      lookForHandle,
+		VisualizeTexts:     visualizeTexts,
+		Filter:             filter,
+		SourceID:           sourceID,
+		StartDate:          startDate,
+		EndDate:            endDate,
+		DateRangeAvailable: dateRangeAvailable,
+		VisualizationType:  visualizationType,
 	}
 
 	result := database.Database.Create(insight)
